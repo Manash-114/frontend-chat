761d8798fc7c6169788777c6196b04d6d176ddff
Manash Jyoti Handique <m@gmail.com>
2024-02-29 00:51:23 +0530

    complete message section design

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,21 +2,33 @@ import React, { useState } from "react";
 import { TbCircleDashed } from "react-icons/tb";
 import { BiCommentDetail } from "react-icons/bi";
 import { AiOutlineSearch } from "react-icons/ai";
-import { BsFilter } from "react-icons/bs";
+import { ImAttachment } from "react-icons/im";
+import "./HomePage.css";
+import {
+  BsEmojiSmile,
+  BsFilter,
+  BsMic,
+  BsThreeDotsVertical,
+} from "react-icons/bs";
 import ChatCard from "./ChatCard/ChatCard";
+import MessageCard from "./MessageCard/MessageCard";
 
 const HomePage = () => {
   const [query, setQuerys] = useState(null);
   const [currentChat, setCurrentChat] = useState(null);
+
+  const [content, setContent] = useState(null);
   const handleSearch = () => {};
   const handleClickOnChatCard = () => {
     setCurrentChat(true);
   };
 
+  const handleCreateNewMessage = () => {};
+
   return (
     <div className="relative">
       <div className="py-14 bg-[#00a884] w-full"></div>
-      <div className="flex bg-[#f0f2f5] w-full h-[90vh] absolute top-6 left-6">
+      <div className="flex bg-[#f0f2f5] w-[96vw] h-[90vh] absolute top-[5vh] left-[2vw]">
         {/* leftside */}
         <div className="w-[30%] bg-[#e8e9ec] h-full">
           <div className="w-full">
@@ -52,7 +64,7 @@ const HomePage = () => {
             </div>
 
             {/* all user */}
-            <div className="bg-white overflow-y-scroll h-[76.8vh] px-3">
+            <div className="bg-white overflow-y-scroll h-[72vh] px-3">
               {query &&
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => (
                   <div onClick={handleClickOnChatCard}>
@@ -78,6 +90,66 @@ const HomePage = () => {
                 send and receive message without keeping your phone online.Use
                 whatsapp on up to 4 linked devices and 1 phone at same time
               </p>
+            </div>
+          </div>
+        )}
+
+        {/* message part */}
+        {currentChat && (
+          <div className="w-[70%] relative bg-blue-200">
+            <div className="header absolute top-0 w-full bg-[#f0f2f5]">
+              <div className="flex justify-between">
+                <div className="py-3 space-x-4 flex items-center px-3">
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src="https://cdn.pixabay.com/photo/2024/02/27/02/06/ai-generated-8599226_1280.jpg"
+                    alt=""
+                  />
+                  <p>username</p>
+                </div>
+                <div className="py-3 flex space-x-4 items-center justify-center px-3">
+                  <AiOutlineSearch />
+                  <BsThreeDotsVertical />
+                </div>
+              </div>
+            </div>
+
+            {/* message section */}
+            <div className="px-5 h-[85vh] overflow-x-scroll ">
+              <div className="space-y-1 flex flex-col justify-center  mt-20 py-2">
+                {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map(
+                  (item, i) => (
+                    <MessageCard
+                      isReqUserMessage={i % 2 == 0}
+                      content={"message"}
+                    />
+                  )
+                )}
+              </div>
+            </div>
+            {/* footer part */}
+            <div className="footer absolute bottom-0 w-full py-3 text-2xl bg-[#f0f2f5]">
+              <div className="flex justify-between items-center px-5 relative">
+                <BsEmojiSmile className="cursor-pointer" />
+                <ImAttachment />
+
+                <input
+                  className="py-2 outline-none border-none bg-white pl-4 rounded-md w-[85%]"
+                  type="text"
+                  onChange={(e) => {
+                    setContent(e.target.value);
+                  }}
+                  placeholder="Type message"
+                  value={content}
+                  onKeyPress={(e) => {
+                    if (e.key == "Enter") {
+                      handleCreateNewMessage();
+                      setContent("");
+                    }
+                  }}
+                />
+                <BsMic />
+              </div>
             </div>
           </div>
         )}
